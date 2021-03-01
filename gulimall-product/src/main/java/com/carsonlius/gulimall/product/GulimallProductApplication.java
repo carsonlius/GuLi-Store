ac197ceae37b5513a789c729a657a813b927a757
liusen <liusen@huice.com>
2021-03-01 23:07:27 +0800

    商城 -- 商品模块 测试mysql是否配置成功

--- gulimall-product/src/main/java/com/carsonlius/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/carsonlius/gulimall/product/GulimallProductApplication.java
@@ -1,9 +1,26 @@
 package com.carsonlius.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 整合mybaits-plus
+ *  1> 倒入依赖        <dependency>
+ *             <groupId>com.baomidou</groupId>
+ *             <artifactId>mybatis-plus-boot-starter</artifactId>
+ *             <version>3.3.1</version>
+ *         </dependency>
+ *  2> 配置
+ *      1) 配置数据源（数据库）
+ *          1. 倒入数据库驱动
+ *          2. 配置数据源
+ *      2) 配置mybaits-plus
+ *          1. @MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+ *          2. 映射文件
+ * */
 @SpringBootApplication
+@MapperScan("com.carsonlius.gulimall.product.dao")
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/test/java/com/carsonlius/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/carsonlius/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,44 @@
 package com.carsonlius.gulimall.product;
 
+import com.baomidou.mybatisplus.core.conditions.AbstractWrapper;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
+import com.carsonlius.gulimall.product.entity.BrandEntity;
+import com.carsonlius.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
+
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setLogo("狼🐺");
+//        brandEntity.setName("saven");
+//        brandEntity.setDescript("知名品牌");
+//        brandEntity.setSort(10);
+//        brandEntity.setShowStatus(1);
+//        brandEntity.setFirstLetter("w");
+//        brandService.save(brandEntity);
+
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setName("anta");
+//        brandService.updateById(brandEntity);
+
+        Wrapper wrapper = new QueryWrapper<BrandEntity>().ge("brand_id", 1);
+        List<BrandEntity>  brandEntityList= brandService.list(wrapper);
+        System.out.println("success---------------------------->!");
+        System.out.println(brandEntityList);
+        System.out.println("===================================");
+
     }
 
 }
