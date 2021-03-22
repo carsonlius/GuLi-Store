323e12680b7ef2cef63f93b8936912206b7b0a62
liusen <liusen@huice.com>
2021-03-22 19:54:43 +0800

    产品三级分类

--- gulimall-product/src/main/java/com/carsonlius/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/carsonlius/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.carsonlius.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +21,57 @@ import com.carsonlius.gulimall.product.service.CategoryService;
 
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Override
+    public List<CategoryEntity> listsWithTree() {
+        // 查所有分类
+        List<CategoryEntity> entityList = baseMapper.selectList(null);
+
+        // todo 组装成树
+        List<CategoryEntity> categoryEntityList = entityList
+                .stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map((menu) -> {
+                    menu.setChildren(getChildren(menu, entityList));
+                    return menu;
+                }).sorted((menu1,menu2) ->{
+                    if (menu1.getSort() == null) {
+                        return -1;
+                    }
+
+                    if (menu2.getSort() == null) {
+                        return 1;
+                    }
+
+                    return menu1.getSort() - menu2.getSort();
+                })
+                .collect(Collectors.toList());
+
+        return categoryEntityList;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        // 设置递归基
+        return all.stream().
+                filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId()))
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildren(categoryEntity, all));
+                    return categoryEntity;
+                }).sorted((menu1,menu2) ->{
+                    if (menu1.getSort() == null) {
+                        return -1;
+                    }
+
+                    if (menu2.getSort() == null) {
+                        return 1;
+                    }
+
+                    return menu1.getSort() - menu2.getSort();
+                })
+                .collect(Collectors.toList());
+    }
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
